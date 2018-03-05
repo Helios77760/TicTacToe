376eb64955f8c8b3a3f7377771537cc816846666
Kitei <jeremy.meyna@hotmail.fr>
2018-03-05 14:56:51 +0100

    Ajout de la fonction threshold

--- tictactoefinder/src/main/java/tictactoefinder/Preparation.java
+++ tictactoefinder/src/main/java/tictactoefinder/Preparation.java
@@ -1,7 +1,9 @@
 package tictactoefinder;
 
+import net.imglib2.RandomAccess;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
 import net.imglib2.type.numeric.real.DoubleType;
 
 public abstract class Preparation extends Step{
@@ -16,21 +18,62 @@ public abstract class Preparation extends Step{
         return res;
     }
 
+
+
+    /**
+     * Fonction threshold
+     * @param img
+     * @return res la nouvelle image avec le Seuillage
+     */
     public static Img<DoubleType> threshold(Img<DoubleType> img)
     {
+
+        // Récupère la dimension de l'image
         long[] imgSize = getDimensions(img);
+
+        // Créer une nouvelle image de la même dimension
         Img<DoubleType> res = ArrayImgs.doubles(imgSize);
 
-        //TODO
-        res = img.copy();
+        // Tableau de déplacement
+        long[] posImg={0,0};
+
+
+        // Niveau de Seuillage
+        double seuil=125;
+
+        // Préparation des curseurs de lecture et d'écriture pour les images
+        RandomAccess<DoubleType> cursorIn = img.randomAccess();
+        RandomAccess<DoubleType> cursorOut = res.randomAccess();
+
+        for(int x = 0; x<imgSize[0];x++){
+            for(int y = 0; y<imgSize[1] ; y++){
+
+                //Affectation de la position actuelle dans un tableau
+                posImg[0]=x;
+                posImg[1]=y;
+
+                //Positionnement du curseur
+                cursorIn.setPosition(posImg);
+                cursorOut.setPosition(posImg);
+
+                if(cursorIn.get().getRealDouble() < seuil)
+                    //Affectation de la nouvelle valeur au curseur : 0 (noir)
+                    cursorOut.get().setReal(0);
+                else
+                    //Affectation de la nouvelle valeur au curseur : 255 (blanc)
+                    cursorOut.get().setReal(255);
+            }
+        }
 
         return res;
     }
+
 
     public static Img<DoubleType> clean(Img<DoubleType> img)
     {
         long[] imgSize = getDimensions(img);
         Img<DoubleType> res = ArrayImgs.doubles(imgSize);
+
 
         //TODO
         res = img.copy();
