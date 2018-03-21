b74c20e36aa2c8a7bd33222bed96f1a0295d2aca
Helios77760 <helios77@outlook.fr>
2018-03-21 15:33:22 +0100

    Otsu threshold

--- tictactoefinder/src/main/java/tictactoefinder/Preparation.java
+++ tictactoefinder/src/main/java/tictactoefinder/Preparation.java
@@ -120,7 +120,7 @@ public abstract class Preparation extends Step{
 
 
         // Niveau de Seuillage
-        double seuil=125;
+        double seuil=determineThreshold(img);
 
         // Préparation des curseurs de lecture et d'écriture pour les images
         RandomAccess<DoubleType> cursorIn = img.randomAccess();
@@ -147,6 +147,66 @@ public abstract class Preparation extends Step{
         }
 
         return res;
+    }
+
+    private static int determineThreshold(Img<DoubleType> img) {
+
+        long[] imgSize = getDimensions(img);
+        int[] histogram = new int[256];
+        RandomAccess<DoubleType> cur = img.randomAccess();
+        long[] pos = {0,0};
+        double value;
+        int finalValue;
+        for(pos[0]=0; pos[0]<imgSize[0];pos[0]++)
+        {
+            for(pos[1]=0; pos[1]<imgSize[1];pos[1]++)
+            {
+                cur.setPosition(pos);
+                value = cur.get().getRealDouble();
+                if(value <= 0)
+                {
+                    finalValue=0;
+                }else if(value >= 255)
+                {
+                    finalValue=255;
+                }else
+                {
+                    finalValue = (int)value;
+                }
+                histogram[finalValue]++;
+            }
+        }
+        long numberOfPixels = imgSize[0]*imgSize[1];
+        int threshold, bestThreshold=0;
+        double numb=0, numf=0, sumb=0, sumf=0, bestValue=0, wb, wf, mb, mf, current;
+        for(int i=0; i<histogram.length;i++)
+        {
+            numf += histogram[i];
+            sumf += i*histogram[i];
+        }
+        for(threshold=1; threshold<256;threshold++)
+        {
+            numb+=histogram[threshold-1];
+            numf-=histogram[threshold-1];
+            sumb+=(threshold-1)*histogram[threshold-1];
+            sumf-=(threshold-1)*histogram[threshold-1];
+
+            wb = numb/numberOfPixels;
+            wf = numf/numberOfPixels;
+            mb = numb != 0 ? sumb/numb : 0;
+            mf = numf != 0 ? sumf/numf : 0;
+
+            current = wb*wf*(mb-mf)*(mb-mf);
+
+            if(current >= bestValue)
+            {
+                bestValue=current;
+                bestThreshold=threshold;
+            }
+
+
+        }
+        return bestThreshold;
     }
 
 
